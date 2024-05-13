ab055b28480527caf975b2fb5f912ded29b82bf7
Chetan <chetantheengineer@gmail.com>
2024-05-13 09:04:07 +0530

    Larger number among 3 numbers

---
+++ 3.largerNumber.js
@@ -0,0 +1,12 @@
+
+function fn(n1, n2, n3) {
+    if (n1 > n2 && n1 > n2) {
+        return n1;
+    } else if(n2 > n1 && n2 > n2) {
+        return n2;
+    } else {
+        return n3;
+    }
+}
+
+console.log(fn(10, 15, 25));
