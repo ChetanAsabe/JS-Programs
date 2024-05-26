9ccc7495fa99d8dc99451c3a9b42378238a42c14
Chetan <chetantheengineer@gmail.com>
2024-05-26 16:20:25 +0530

    Compare two strings

---
+++ 11.CompareTwoStrings.js
@@ -0,0 +1,9 @@
+const string1 = "Chetan";
+const string2 = "chetan";
+
+const reseult = string1.toLowerCase() === string2.toLowerCase();
+
+if(reseult) 
+    console.log('Two Strings are Similar.');
+else
+    console.log('Two Strings are not Similar.');
